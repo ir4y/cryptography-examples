174c13ed64de6a39917a6885fb12f8f3202d293a
Ilya Beda <ir4y.ix@gmail.com>
2014-03-15 22:39:04 +0800

    Optimize controll flow, add guards

--- haskell/caesar.hs
+++ haskell/caesar.hs
@@ -25,12 +25,16 @@ usage   = putStrLn "Usage: encode [-cv] -k key -f file"
 exit    = exitWith ExitSuccess
 die     = exitWith (ExitFailure 1)
 
-parse ["-c", "-k", key, "-f", fs] = do { content <- readFile fs
-                                       ; putStr (caesar (read key) content)
-                                       }
-parse ["-v", "-k", key, "-f", fs] = do { content <- readFile fs
-                                       ; putStr (vigener key content)
-                                       }
-parse _                           = usage
+encript "-c" key content = caesar (read key) content
+encript "-v" key content = vigener key content
+
+
+parse [cipher, "-k", key, "-f", fs]
+    | (cipher == "-c" && (and $ map isDigit key))
+      ||
+      (cipher == "-v" && (and $ map (`elem` alphabet) key)) = do { content <- readFile fs
+                                                                 ; putStr (encript cipher key content)
+                                                                 }
+parse _                                                     = usage
  
 main = getArgs >>= parse >> exit
