f803f2a7a4c01486b7ecea5a21941101f49297ad
Ilya Beda <ir4y.ix@gmail.com>
2014-03-15 21:14:47 +0800

    Filter alphabet

--- haskell/caesar.hs
+++ haskell/caesar.hs
@@ -3,16 +3,33 @@ import Data.List
 import System.Environment
 import System.Exit
 
-encode shift msg = map (chr . (+ shift) . ord) msg
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-usage   = putStrLn "Usage: encode -k key -f file"
+do_plus shift char
+    | (shift + char) > ord('z') = shift + char - 26
+    | (shift + char) < ord('a') = shift + char + 26
+    | otherwise = shift + char
+
+do_shift shift char 
+    | char `elem` map ord alphabet = shift `do_plus` char
+    | otherwise = char
+
+caesar shift msg = map (chr . (do_shift shift) . ord) msg
+
+vigener key msg = msg
+
+usage   = putStrLn "Usage: encode [-cv] -k key -f file"
 exit    = exitWith ExitSuccess
 die     = exitWith (ExitFailure 1)
 
-parse ["-k", key, "-f", fs] = do { content <- readFile fs
-                                 ; putStr (encode (read key) content)
-                                 ; exit
-                                 }
-parse _                     = usage >> exit
+parse ["-c", "-k", key, "-f", fs] = do { content <- readFile fs
+                                       ; putStr (caesar (read key) content)
+                                       ; exit
+                                       }
+parse ["-v", "-k", key, "-f", fs] = do { content <- readFile fs
+                                       ; putStr (vigener key content)
+                                       ; exit
+                                       }
+parse _                           = usage >> exit
 
 main = getArgs >>= parse
